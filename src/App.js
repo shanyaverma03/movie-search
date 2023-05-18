b7b945d05563abea36dde2e9c1d2e42f5cc54f03
shanya <shanyaverma03@gmail.com>
2023-05-18 12:48:51 +0530

    added main nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./Pages/RootLayout";
+import HomePage from "./Pages/HomePage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+    ],
   },
 ]);
 function App() {
---
+++ src/Pages/Favourites.js
@@ -0,0 +1,5 @@
+const Favourites = () => {
+  return <h1>Favsssss</h1>;
+};
+
+export default Favourites;
