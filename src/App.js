30e4a8bfe4753cda42c65c575c9d02be7f7782f1
shanya <shanyaverma03@gmail.com>
2023-05-18 12:50:53 +0530

    added path for favourites

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./Pages/RootLayout";
 import HomePage from "./Pages/HomePage";
+import Favourites from "./Pages/Favourites";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <HomePage />,
+      },
+      {
+        path: "favourites",
+        element: <Favourites />,
       },
     ],
   },
