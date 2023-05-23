fe89913a4f6c72c6a9430127a06d3c0d21543040
shanya <shanyaverma03@gmail.com>
2023-05-24 00:15:14 +0530

    reduced code in main nav

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -10,21 +10,21 @@ import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 
 const pages = ["Browse", "My List"];
 function MainNavigation() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const navigate = useNavigate();
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
+  const browseClickHandler = () => {
+    navigate("browse");
   };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
+  const mylistClickHandler = () => {
+    navigate("mylist");
   };
 
   return (
@@ -49,83 +49,22 @@ function MainNavigation() {
             Movie Search
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">
-                    <Link
-                      style={{ textDecoration: "none", color: "white" }}
-                      to={page.replace(/ +/g, "").toLowerCase()}
-                    >
-                      {page}
-                    </Link>
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                <Link
-                  style={{ textDecoration: "none", color: "white" }}
-                  to={page.replace(/ +/g, "").toLowerCase()}
-                >
-                  {page}
-                </Link>
-              </Button>
-            ))}
+            <Button
+              key="browse"
+              onClick={browseClickHandler}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              Browse
+            </Button>
+
+            <Button
+              key="mylist"
+              onClick={mylistClickHandler}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              My List
+            </Button>
           </Box>
           <Link to="login" style={{ textDecoration: "none", color: "white" }}>
             <Button color="inherit">Login</Button>
