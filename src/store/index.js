0f112007a2d2cafa38f4b406bf711753f39b46a3
shanya <shanyaverma03@gmail.com>
2023-05-23 23:58:10 +0530

    created isAuthenticated slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import movieSlice from "./selectedMovieSlice";
+import isAuthenticatedSlice from "./isAuthenticatedSlice";
 
 const store = configureStore({
-  reducer: movieSlice.reducer,
+  reducer: {
+    selectedMovie: movieSlice.reducer,
+    isAuthenticated: isAuthenticatedSlice.reducer,
+  },
 });
 
 export const movieActions = movieSlice.actions;
+export const isAuthenticatedActions = isAuthenticatedSlice.actions;
 export default store;
--- src/store/isAuthenticatedSlice.js
+++ src/store/isAuthenticatedSlice.js
@@ -1,1 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = { isAuthenticated: false };
+
+const isAuthenticatedSlice = createSlice({
+  name: "isAuthenticated",
+  initialState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export default isAuthenticatedSlice;
