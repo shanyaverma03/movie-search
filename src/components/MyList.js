7f44e4c71c527feedfb35ecfe8a92cbada8663ee
shanya <shanyaverma03@gmail.com>
2023-05-24 02:40:25 +0530

    show my list movies in a card format

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,11 +1,40 @@
 import { useSelector } from "react-redux";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 const MyList = () => {
   const myList = useSelector((state) => state.mylist.mylist);
   return (
     <>
       <h1>the list</h1>
       {myList.map((movie) => (
-        <p key={movie.id}>{movie.title}</p>
+        <Card key={movie.id} sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            sx={{ height: 500 }}
+            image={movie.poster}
+            title="movie-detail"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {movie.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Rank: {movie.rank}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Something about the movie- to figure out
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Remove from list</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
       ))}
     </>
   );
