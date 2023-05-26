aa36a5388d13607d990a01f10c5b21b89ba46a45
Shanya Verma <shanya@Shanyas-MacBook-Air.local>
2023-05-27 02:04:28 +0530

    add footer

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -8,6 +8,7 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router";
 import classes from "./Homescreen.module.css";
+import Footer from "./Footer";
 const Homescreen = () => {
   const navigate = useNavigate();
 
@@ -21,47 +22,50 @@ const Homescreen = () => {
     augmentColor({ color: { main: mainColor } });
 
   return (
-    <main className={classes.wrapper}>
-      {/* Hero unit */}
-      <Box
-        sx={{
-          bgcolor: `linear-gradient(
+    <div>
+      <main className={classes.wrapper}>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: `linear-gradient(
             90deg,
             rgba(2, 0, 36, 1) 0%,
             rgba(92, 112, 128, 1) 23%,
             rgba(85, 122, 149, 0.9710477941176471) 64%
           )`,
-          pt: 8,
-          pb: 6,
-        }}
-      >
-        <Container maxWidth="sm">
-          <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            color="#F9F6EE"
-            gutterBottom
-          >
-            Movie Search
-          </Typography>
-          <Typography variant="h5" align="center" color="#FAF9F6" paragraph>
-            Search the ratings and description of any movie or TV show you want!
-            Add your favourite movies to your list. Happy browsing!
-          </Typography>
-          <Stack
-            sx={{ pt: 4 }}
-            direction="row"
-            spacing={2}
-            justifyContent="center"
-          >
-            <Button variant="contained" onClick={browseMovieHandler}>
-              Browse a movie
-            </Button>
-          </Stack>
-        </Container>
-      </Box>
-    </main>
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="#F9F6EE"
+              gutterBottom
+            >
+              Movie Search
+            </Typography>
+            <Typography variant="h5" align="center" color="#FAF9F6" paragraph>
+              Search the ratings and description of any movie or TV show you
+              want! Add your favourite movies to your list. Happy browsing!
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained" onClick={browseMovieHandler}>
+                Browse a movie
+              </Button>
+            </Stack>
+          </Container>
+        </Box>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
