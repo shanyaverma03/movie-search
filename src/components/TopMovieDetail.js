03b12fc6e98ba6001a688459eafc3d6c2dc3b9f7
Shanya Verma <shanya@Shanyas-MacBook-Air.local>
2023-05-31 17:57:16 +0530

    add the top movie to list

--- src/components/TopMovieDetail.js
+++ src/components/TopMovieDetail.js
@@ -3,18 +3,34 @@ import Button from "./UI/Button";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import { useState, useEffect } from "react";
+import { addToMyListAction } from "../store/myListSlice";
+import { useDispatch } from "react-redux";
 
 const TopMovieDetail = (props) => {
   const [movieAlreadyInList, setAlreadyMovieInList] = useState(false);
   const isAuthenticated = useSelector(
     (state) => state.isAuthenticated.isAuthenticated
   );
+  const userId = useSelector((state) => state.isAuthenticated.userId);
   const myList = useSelector((state) => state.mylist.mylist);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const addToListHandler = () => {
     if (isAuthenticated) {
       //add to list
+
+      const movieToBeAdded = {
+        id: props.imdbid,
+        poster: props.image,
+        rank: props.rank,
+        title: props.title,
+        userId: userId,
+        year: props.year,
+        type: props.genre,
+      };
+      dispatch(addToMyListAction(movieToBeAdded, userId, movieToBeAdded.id));
+      navigate("/mylist");
     } else {
       navigate("/login");
     }
