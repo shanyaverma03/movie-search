b11cf29719c18d7b90756ca78323d16bcc1ec581
Shanya Verma <shanya@Shanyas-MacBook-Air.local>
2023-05-31 17:49:32 +0530

    render add to list button conditionally in cards

--- src/components/TopMovieDetail.js
+++ src/components/TopMovieDetail.js
@@ -1,7 +1,37 @@
 import classes from "./TopMovieDetail.module.css";
 import Button from "./UI/Button";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { useState, useEffect } from "react";
 
 const TopMovieDetail = (props) => {
+  const [movieAlreadyInList, setAlreadyMovieInList] = useState(false);
+  const isAuthenticated = useSelector(
+    (state) => state.isAuthenticated.isAuthenticated
+  );
+  const myList = useSelector((state) => state.mylist.mylist);
+  const navigate = useNavigate();
+
+  const addToListHandler = () => {
+    if (isAuthenticated) {
+      //add to list
+    } else {
+      navigate("/login");
+    }
+  };
+
+  const goToListHandler = () => {
+    navigate("/mylist");
+  };
+
+  useEffect(() => {
+    const indexOfMovie = myList.some((movie) => movie.title === props.title);
+    if (indexOfMovie) {
+      //movie already in the list
+      setAlreadyMovieInList(true);
+    }
+  }, [myList, props.title]);
+
   return (
     <div className={classes.topMovies}>
       <div className={classes.card}>
@@ -9,7 +39,11 @@ const TopMovieDetail = (props) => {
         <div className={classes.container}>
           <p>{props.rating}</p>
           <p>{props.title}</p>
-          <button>Add to list</button>
+          {movieAlreadyInList ? (
+            <button onClick={goToListHandler}>Go to list</button>
+          ) : (
+            <button onClick={addToListHandler}>Add to list</button>
+          )}
 
           <div>Trailer</div>
           <div>logo</div>
